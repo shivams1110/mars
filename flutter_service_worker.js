0bd8c5319936311d4652d3dea31c3f4960916f37
Shivam Sharma <shivams1110@live.in>
2022-09-06 12:59:50 +0530

    Add files via upload
    login

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9eb6b5662cf3fc64d8de8a2dabe48cbb",
-"index.html": "b445f81d0b0aaf9d954490efc2a3c92e",
-"/": "b445f81d0b0aaf9d954490efc2a3c92e",
+"index.html": "832ffe17c94bce5bf29a7e2e03592d76",
+"/": "832ffe17c94bce5bf29a7e2e03592d76",
 "main.dart.js": "facd2eb94e19bff1fcd2e2896583c329",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
